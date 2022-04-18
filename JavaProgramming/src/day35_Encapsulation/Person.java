cffee8f1faaf533d5a7925a64a6757ef1142f882
kaankilic <avkaankilic@gmail.com>
2022-04-19 00:34:36 +0300

    create a new class: Person

--- JavaProgramming/src/day35_Encapsulation/Person.java
+++ JavaProgramming/src/day35_Encapsulation/Person.java
@@ -23,4 +23,26 @@ public class Person {
         numberOfHead=1;
         numberOfWings=0;
     }
+
+    public static void printPlanetName(){
+        System.out.println("Planet name is "+planet);
+    }
+
+    public void eat(String food){
+        System.out.println(name+" is eating "+food);
+    }
+
+    public void drink(String drink){
+        System.out.println(name+" is drinking "+drink);
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", language='" + language + '\'' +
+                ", planet='" + planet + '\'' +
+                '}';
+    }
 }
